c9b1f64d228949d3023fe4650b7a6ec146e331cf
Stephan Schneider <stephanschndr@gmail.com>
2016-02-27 18:08:21 +0100

    refactor `Room` class into own file :arrow_right:

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const io = Socket()
 const low = require('lowdb')
 const storage = require('lowdb/file-async')
 
+import Room, { slug } from './room'
+
 import { userConnected, userNamechange, userDisconnected, unlocked } from '../actions/room'
 import { joined, authenticated, setName, kicked } from '../actions/user'
 import { ticketInfo, userVote } from '../actions/pm'
@@ -18,101 +20,6 @@ import * as actions from '../actions/server'
 const db = low('db.json', { storage })
 
 const rooms = {}
-
-function slug (name) {
-  return name.toLowerCase()
-}
-
-class Room {
-  constructor (name) {
-    this.name = name
-    this.slug = slug(name)
-    this.users = []
-    this.round = {}
-  }
-
-  addUser (user) {
-    const exists = this.findUser(user)
-
-    if (!exists) {
-      this.users = [...this.users, user]
-    }
-
-    return this
-  }
-
-  findUser (user) {
-    return this.users.find((u) => u.socket === user.socket)
-  }
-
-  updateUser (user) {
-    const current = this.findUser(user)
-
-    Object.keys(user)
-      .filter((key) => key !== 'socket')
-      .forEach((key) => {
-        current[key] = user[key]
-      })
-
-    return current
-  }
-
-  removeUser (user) {
-    const users = this.users
-      .filter((u) => u.socket !== user.socket)
-
-    this.users = users
-
-    return this
-  }
-
-  startVoteRound () {
-    this.round = { votes: {}, finished: false }
-  }
-
-  setRoundVote (user, value) {
-    this.round.votes[user] = value
-  }
-
-  roundFinished () {
-    const users = this.users.filter((u) => !u.pm)
-    const finished = Object.keys(this.round.votes).length === users.length
-
-    if (finished) {
-      this.round.finished = true
-    }
-
-    return finished || this.round.finished
-  }
-
-  getRecommendedPoints () {
-    // Object of point objects containing amount of votes.
-    const allPoints = Object.keys(this.round.votes).reduce((points, user) => {
-      const userVote = Number.parseInt(this.round.votes[user], 10)
-
-      points[userVote] = points[userVote] || 0
-      points[userVote]++
-
-      return points
-    }, {})
-
-    // Minimum amount of two because a recommendation for a point with only one
-    // user is not really helpful.
-    let maxVotes = 2
-    // Array of recommended points.
-    const recommended = Object.keys(allPoints).reduce((all, point) => {
-      if (allPoints[point] < maxVotes) {
-        return all
-      }
-
-      maxVotes = allPoints[point]
-
-      return [...all, point]
-    }, [])
-
-    return recommended
-  }
-}
 
 function claimRoom (name, token) {
   let room = db('rooms').find({ name })
---
+++ server/room.js
@@ -0,0 +1,94 @@
+export function slug (name) {
+  return name.toLowerCase()
+}
+
+export default class Room {
+  constructor (name) {
+    this.name = name
+    this.slug = slug(name)
+    this.users = []
+    this.round = {}
+  }
+
+  addUser (user) {
+    const exists = this.findUser(user)
+
+    if (!exists) {
+      this.users = [...this.users, user]
+    }
+
+    return this
+  }
+
+  findUser (user) {
+    return this.users.find((u) => u.socket === user.socket)
+  }
+
+  updateUser (user) {
+    const current = this.findUser(user)
+
+    Object.keys(user)
+      .filter((key) => key !== 'socket')
+      .forEach((key) => {
+        current[key] = user[key]
+      })
+
+    return current
+  }
+
+  removeUser (user) {
+    const users = this.users
+      .filter((u) => u.socket !== user.socket)
+
+    this.users = users
+
+    return this
+  }
+
+  startVoteRound () {
+    this.round = { votes: {}, finished: false }
+  }
+
+  setRoundVote (user, value) {
+    this.round.votes[user] = value
+  }
+
+  roundFinished () {
+    const users = this.users.filter((u) => !u.pm)
+    const finished = Object.keys(this.round.votes).length === users.length
+
+    if (finished) {
+      this.round.finished = true
+    }
+
+    return finished || this.round.finished
+  }
+
+  getRecommendedPoints () {
+    // Object of point objects containing amount of votes.
+    const allPoints = Object.keys(this.round.votes).reduce((points, user) => {
+      const userVote = Number.parseInt(this.round.votes[user], 10)
+
+      points[userVote] = points[userVote] || 0
+      points[userVote]++
+
+      return points
+    }, {})
+
+    // Minimum amount of two because a recommendation for a point with only one
+    // user is not really helpful.
+    let maxVotes = 2
+    // Array of recommended points.
+    const recommended = Object.keys(allPoints).reduce((all, point) => {
+      if (allPoints[point] < maxVotes) {
+        return all
+      }
+
+      maxVotes = allPoints[point]
+
+      return [...all, point]
+    }, [])
+
+    return recommended
+  }
+}
