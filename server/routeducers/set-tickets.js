0e33f8cc62ed59770e627b13242f4634d1dcb701
yannickoo <github@yannick.md>
2016-02-29 13:28:07 +0100

    removes set-tickets routeducer

--- server/routeducers/set-tickets.js
+++
@@ -1,9 +0,0 @@
-import { ticketList } from '../../actions/pm'
-
-export default function setTickets ({ socket, action, rooms }) {
-  const { tickets } = action
-
-  console.log(tickets.length, 'tickets have been set.')
-
-  socket.emit('action', ticketList(tickets))
-}
