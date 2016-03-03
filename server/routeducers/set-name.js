49f33dcd32bc4ac028a4c34280cc2147ba735a21
yannickoo <github@yannick.md>
2016-03-03 15:50:48 +0100

    checks whether room exists before trying to send messages to it :hammer:

--- server/routeducers/set-name.js
+++ server/routeducers/set-name.js
@@ -4,10 +4,13 @@ import { setName as changeName } from '../../actions/user'
 export default function setName ({ socket, action, rooms }) {
   const { name } = action
   const room = rooms[socket.room]
-  const user = room.updateUser({ name, socket: socket.id })
+
+  if (room) {
+    const user = room.updateUser({ name, socket: socket.id })
+    socket.broadcast.to(socket.room).emit('action', userNamechange(user))
+  }
 
   console.log(`${socket.username} ➝  ${name}`)
 
-  socket.broadcast.to(socket.room).emit('action', userNamechange(user))
-  socket.emit('action', changeName(user.name))
+  socket.emit('action', changeName(name))
 }
